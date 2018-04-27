c70f304101058a96a2db40267262b9bab3e72217
Joshua Mengote <mengotejoshm@gmail.com>
2018-04-27 15:37:45 +0800

    Add utility methods for user input
    Add validation of date input
    Add validation for mobile number input
    Add validation for landline number input

---
+++ utility/src/main/java/ecc/hibernate/xml/util/UserInputUtils.java
@@ -0,0 +1,164 @@
+package ecc.hibernate.xml.util;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import org.apache.commons.lang3.StringUtils;
+
+public class UserInputUtils {
+    private static Scanner scan = new Scanner(System.in);
+    
+    public static int numberWithLimit (String header, int numberOfChoices) {
+        int optionSelected = 0;
+        boolean inputValid = false;
+        while(!inputValid) {
+            try {
+                System.out.print(header + " (" + numberOfChoices + " choices): ");
+                String readString = scan.nextLine();
+                if (StringUtils.isBlank(readString)) {
+                    System.out.println("\tEnter a number.");
+                } else {
+                    optionSelected = Integer.parseInt(readString);
+                    if (optionSelected > numberOfChoices || optionSelected <= 0){
+                        System.out.println("\tInput not within option.");
+                    } else {
+                        inputValid = true;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("\tInvalid Input! Input not a number!");
+            }
+        }
+        return optionSelected;
+    }
+    
+    public static int number (String header) {
+        int input = 0;
+        boolean inputValid = false;
+        while(!inputValid) {
+            try {
+                System.out.print(header);
+                String readString = scan.nextLine();
+                if (StringUtils.isBlank(readString)) {
+                    System.out.println("\tEnter a number.");
+                } else {
+                    input =  Integer.parseInt(readString);
+                    inputValid = true;
+                }
+            } catch (NumberFormatException e) {
+                    System.out.println("\tInvalid Input! Input not a number!");
+                }
+        }
+        return input;
+    }
+
+    public static float decimal (String header) {
+        float input = 0;
+        boolean inputValid = false;
+        while(!inputValid) {
+            try {
+                System.out.print(header);
+                String readString = scan.nextLine();
+                if (StringUtils.isBlank(readString)) {
+                    System.out.println("\tEnter a number.");
+                } else {
+                    input =  Float.parseFloat(readString);
+                    inputValid = true;
+                }
+            } catch (NumberFormatException e) {
+                    System.out.println("\tInvalid Input! Input not a number!");
+                }
+        }
+        return input;
+    }
+    
+    public static String string (String header) {
+        String inputString = "";
+        boolean validInput = false;
+        while(!validInput) {
+            System.out.print(header);
+            inputString = scan.nextLine();
+            if (StringUtils.isBlank(inputString)) {
+                System.out.println("\t Enter a string.");
+                validInput = false;
+            } else {
+                validInput = true;
+            }
+        }
+        return inputString;
+    }
+
+    public static String stringWithNull (String header) {
+        String inputString = "";
+        boolean validInput = false;
+        while(!validInput) {
+            System.out.print(header);
+            inputString = scan.nextLine();
+            if (StringUtils.isBlank(inputString)) {
+                System.out.print("\t Leave blank? (Y/N): ");
+                String confirmationChar = scan.nextLine();
+                if (confirmationChar.equalsIgnoreCase("Y")) {
+                    inputString = "";
+                    break;
+                }
+                validInput = false;
+            } else {
+                validInput = true;
+            }
+        }
+        return inputString;
+    }
+
+    public static Date date(String header) {
+        Date date = new Date();
+
+        System.out.print(header);
+        String userDate = scan.nextLine();
+                  
+         String pattern = "MM/dd/yyyy";
+         SimpleDateFormat sdf = new SimpleDateFormat();
+         try {
+            sdf.applyPattern(pattern);
+            sdf.setLenient(false);
+            date = sdf.parse(userDate);
+         } catch (ParseException e) {
+            System.out.println("\t No such date. Please try again.");
+            date(header);
+         }
+        return date;
+    }
+    
+    public static String mobileNumber (String header) {
+        String inputString = "";
+        boolean validInput = false;
+        while(!validInput) {
+            System.out.print(header);
+            inputString = scan.nextLine();
+            if (inputString.matches("09\\d{9}")) {
+                validInput = true;
+            } else {
+                System.out.println("\t Enter a local mobile number [Pattern: 09XXXXXXXXX]. Please try again.");
+            }
+        }
+        return inputString;
+    }
+
+    public static String landline (String header) {
+        String inputString = "";
+        boolean validInput = false;
+        while(!validInput) {
+            System.out.print(header);
+            inputString = scan.nextLine();
+            if (inputString.matches("\\d{7}")) {
+                validInput = true;
+            } else {
+                System.out.println("\t Enter a local landline [Pattern: XXX-XXXX]. Please try again.");
+            }
+        }
+        return inputString;
+    }
+
+}
