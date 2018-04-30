16602343b477d101a382389bc49f433a04fc323c
Joshua Mengote <mengotejoshm@gmail.com>
2018-04-30 10:28:06 +0800

    Modify hibernate configuration to use annotated class for mapping instead of xml mapping files
    Remove mapping resource, replaced by mapping class (in hibernate.cfg.xml)
    Replace configure object to annotation configuration (in HibernateUtils)

--- utility/src/main/java/ecc/hibernate/xml/util/HibernateUtils.java
+++ utility/src/main/java/ecc/hibernate/xml/util/HibernateUtils.java
@@ -3,13 +3,14 @@ package ecc.hibernate.xml.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.AnnotationConfiguration;
 
 public class HibernateUtils {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 	
 	private static SessionFactory buildSessionFactory() {
 		try{
-			Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+			Configuration configuration = new AnnotationConfiguration().configure();
 			return configuration.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build());
 		} catch (Throwable ex) {
 			System.err.println("Initial SessionFactory creation failed" + ex);
