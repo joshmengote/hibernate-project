8fa65762869a1a97aa01802a23e84dfc05bb8f8a
Joshua Mengote <mengotejoshm@gmail.com>
2018-05-03 12:44:56 +0800

    Modify RoleDao to extend GenericDao
    RoleDao overrides the find method of GenericDao

--- core/dao/src/main/java/ecc/hibernate/xml/dao/RoleDao.java
+++ core/dao/src/main/java/ecc/hibernate/xml/dao/RoleDao.java
@@ -1,10 +1,7 @@
 package ecc.hibernate.xml.dao;
 
 import ecc.hibernate.xml.model.Role;
-import ecc.hibernate.xml.util.HibernateUtils;
-
 import java.util.List;
-import java.util.ArrayList;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
@@ -14,88 +11,24 @@ import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 
 
-public class RoleDao {
-    private static Session session;
-    private static Transaction transaction;
-    private static HibernateUtils hibernateUtil = new HibernateUtils();
-    private void startOperation() throws HibernateException {
-        session = hibernateUtil.getSessionFactory().openSession();
-        transaction = session.beginTransaction();
+public class RoleDao extends GenericDao {
+    public RoleDao() {
+        super();
     }
-
-    public void saveOrUpdate(Role role) {
-        try {
-            startOperation();
-            session.saveOrUpdate(role);
-            transaction.commit();
-        } catch (HibernateException e) {
-            transaction.rollback();
-        } finally {
-            session.close();
-        }
-    }
-
+    
     public Role find(Long id) {
         Role role = new Role();
         try {
             startOperation();
             role = (Role) session.get(Role.class, id);
             Hibernate.initialize(role.getPerson());
-            transaction.commit();
+            tx.commit();
         } catch (HibernateException e) {
-            transaction.rollback();
+            tx.rollback();
         } finally {
             session.close();
         }
         return role;
-    }
-
-    public List findAll(){
-        List<Role> objects = new ArrayList();
-        try {
-            startOperation();
-            Criteria criteria = session.createCriteria(Role.class);
-            criteria.setCacheable(true);
-            objects = criteria.list();
-            transaction.commit();
-        } catch (HibernateException e) {
-            transaction.rollback();
-        } finally {
-            session.close();
-        }
-        return objects;
-    }   
-
-    public void delete(Role role) {
-        try {
-            startOperation();
-            session.delete(role);
-            transaction.commit();
-        } catch (HibernateException e) {
-            transaction.rollback();
-        } finally {
-            session.close();
-        }
-    }
-
-    public boolean roleExist(String roleName) {
-        boolean exist = false;
-        try {
-            startOperation();
-            Criteria criteria = session.createCriteria(Role.class);
-            criteria.add(Restrictions.eq("roleName", roleName));
-            criteria.setCacheable(true);
-            List list = criteria.list();
-            if (list.size() == 0) {
-                exist = true;
-            }
-            transaction.commit();
-        } catch (HibernateException e) {
-            transaction.rollback();
-        } finally {
-            session.close();
-        }
-        return exist;
-    }
+    }        
     
 }
